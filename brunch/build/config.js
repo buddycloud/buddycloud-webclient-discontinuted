ffc42383a3fafe51ac7f344928b2191cd46f6975
Astro <astro@spaceboyz.net>
2011-09-21 15:22:23 +0200

    config: switch bosh_service back to crater.buddycloud.org
    While metajack works for normal users on all servers, our own allows anonymous
    auth.

--- brunch/build/config.js
+++ brunch/build/config.js
@@ -1,8 +1,8 @@
 
 // export config
 window.config = {
-    //bosh_service: 'http://crater.buddycloud.org:5280/http-bind/',
-    bosh_service: 'http://bosh.metajack.im:5280/xmpp-httpbind',
+    bosh_service: 'http://crater.buddycloud.org:5280/http-bind/',
+    //bosh_service: 'http://bosh.metajack.im:5280/xmpp-httpbind',
     home_domain: "buddycloud.org",
     anon_domain: "anon.buddycloud.com",
     /* For registration: */
