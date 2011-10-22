ab248e18e14b12fb93683c8c02e10ee08e223fc3
Simon Tennant <simon@buddycloud.com>
2011-10-22 11:34:28 +0200

    actually use SSL
    pointless to use SSL if we pipe all messages back on a non encrypted
    channel.

--- brunch/build/config.js
+++ brunch/build/config.js
@@ -1,7 +1,7 @@
 
 // export config
 window.config = {
-    bosh_service: 'http://crater.buddycloud.org:5280/http-bind/',
+    bosh_service: 'https://beta.buddycloud.com:443/http-bind/',
     //bosh_service: 'http://bosh.metajack.im:5280/xmpp-httpbind',
     home_domain: "buddycloud.org",
     anon_domain: "anon.buddycloud.org",
