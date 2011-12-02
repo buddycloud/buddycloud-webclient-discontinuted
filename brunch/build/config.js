508d4ee4a1c778361e6da2c81e275b837a61f0ac
dodo <dodo@blacksec.org>
2011-12-02 08:19:48 +0100

    add more config documentation

--- brunch/build/config.js
+++ brunch/build/config.js
@@ -4,7 +4,8 @@ window.config = {
 
     /* address of the bosh gateway. this should be reachable from webclient domain */
     bosh_service: 'http://beta.buddycloud.org:5280/http-bind/',
-    //bosh_service: 'http://bosh.metajack.im:5280/xmpp-httpbind',
+    //bosh_service: 'https://beta.buddycloud.org:443/http-bind/', // secure
+    //bosh_service: 'http://bosh.metajack.im:5280/xmpp-httpbind', // just for testing!
 
     /*this is the inbox domain for anon users */
     home_domain: "buddycloud.org",
@@ -12,6 +13,7 @@ window.config = {
     /* domain to authenticate against for anon users */
     anon_domain: "anon.buddycloud.org",
 
-    /* For registration: */
-    domain: "buddycloud.org"
+    /* overall used domain for this webclient instance.
+     * used for registration and login. */
+    domain: "buddycloud.org",
 };
