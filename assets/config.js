e3acb9a4433a4ec6d60ecd1d11bb49fc6ed15164
Astro <astro@spaceboyz.net>
2012-02-15 18:01:56 +0100

    config.js: use example.com for defaults
    Requested by Simon, makes getting started with buddycloud.org a configuration mess.

--- assets/config.js
+++ assets/config.js
@@ -3,23 +3,22 @@
 window.config = {
 
     /* address of the bosh gateway. this should be reachable from webclient domain */
-    bosh_service: 'http://beta.buddycloud.org:5280/http-bind/',
-    //bosh_service: 'https://beta.buddycloud.org:443/http-bind/', // secure
-    //bosh_service: 'http://bosh.metajack.im:5280/xmpp-httpbind', // just for testing!
+    bosh_service: 'https://beta.buddycloud.org:443/http-bind/',
 
     /*this is the inbox domain for anon users */
-    home_domain: "buddycloud.org",
+    home_domain: "example.com",
 
     /* domain to authenticate against for anon users */
-    anon_domain: "anon.buddycloud.org",
+    anon_domain: "anon.example.com",
 
     /* overall used domain for this webclient instance.
      * used for registration and login. */
-    domain: "buddycloud.org",
+    domain: "example.com",
 
     /* Default domain to create topics under
      * unless the user specifies …@domain as the name. */
     topic_domain: "topics.example.com",
 
-    embedly_key: "2c1bedbc2aa111e1acbf4040d3dc5c07"
+    /* Sign up for an embed.ly account to use OStatus */
+    //embedly_key: "xxx"
 };
