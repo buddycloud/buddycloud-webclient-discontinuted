6804a2ca6e8b51bdf95057f8a1b0d658c74f72f4
dodo <dodo.the.last@gmail.com>
2012-08-05 16:41:15 +0200

    clean up markup in homepagetext

--- assets/config.js
+++ assets/config.js
@@ -33,21 +33,30 @@ window.config = {
      * e.g ["web/js/show-client-0.1.0.js"] */
     plugins: [],
 
+    /* mail address of the sysadmin or help desk */
+    helpmail: "your-friendly-sysadmin@example.org",
+
 };
 
 /* text for the homepage */
-window.config['homepage-text'] = "Share, discover, communicate in a magically simple way."
+window.config['homepage-text'] = "<h3>Share, discover, communicate in a magically simple way.</h3>"
     + "<p>"
     + "buddycloud is a new kind of social network. In your buddycloud channel you can communicate "
     + "and share with other thinkers and makers: buddycloud’s easy to understand privacy model and "
     + "strong encryption keep you safe online."
+    + "</p>"
     + "<ul>"
     + "<li>create channels and share them with the world or keep them private</li>"
     + "<li>share your photos, movies and ideas about any subject</li>"
     + "<li>discover great channels from other buddycloud users around the world</li>"
+    + "</ul>"
     + "<p>"
     + "Learn more <a href='http://buddycloud.com/features'>about buddycloud</a>."
+    + "</p>"
     + "<p>"
     + "For help with this buddycloud on" + window.config.domain
-    + "contact <a href='mailto:your-friendly-sysadmin@example.org'>your friendly sysadmin'</a>"
-    + "Coming soon for Android™ and iPhone®.";
+    + "contact <a href='mailto:" + window.config.helpmail + "'>your friendly sysadmin'</a>"
+    + "</p>"
+    + "<p>"
+    + "Coming soon for Android™ and iPhone®."
+    + "</p>";
