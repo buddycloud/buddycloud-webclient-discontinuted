e6464e30c09d90416d365fd3354c1ce333c7cf47
Simon Tennant <simon@buddycloud.com>
2012-08-05 14:42:57 +0300

    default domain text

--- assets/config.js
+++ assets/config.js
@@ -16,6 +16,22 @@ window.config = {
     /* overall used domain for this webclient instance.
      * used for registration and login. */
     domain: "example.com",
+    
+    /* text for the homepage */
+    homepage-text: "Share, discover, communicate in a magically simple way." 
+    + "<p>"
+    + "buddycloud is a new kind of social network. In your buddycloud channel you can communicate "
+    + "and share with other thinkers and makers: buddycloud’s easy to understand privacy model and "
+    + "strong encryption keep you safe online."
+    + "<ul>"
+    + "<li>create channels and share them with the world or keep them private</li>"
+    + "<li>share your photos, movies and ideas about any subject</li>"
+    + "<li>discover great channels from other buddycloud users around the world</li>"
+    + "<p>"
+    + "Learn more <a href='http://buddycloud.com/features'>about buddycloud</a>."
+    + "<p>"
+    + "For help with this buddycloud on" + $domain "contact <a href='mailto:your-friendly-sysadmin@example.org'>your friendly sysadmin'</a>"
+    + "Coming soon for Android™ and iPhone®.",
 
     /* Default domain to create topics under
      * unless the user specifies …@domain as the name. (see: https://buddycloud.org/wiki/Install#Advanced_Topics) */
